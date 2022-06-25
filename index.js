dafe0d4097aeb664bbd87ea070e9a957d676863a
SanicBTW <58010536+SanicBTW@users.noreply.github.com>
2022-06-25 00:20:08 +0000

    Little example

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 setBackground('./Nuevo proyecto.png');
+setGlobalStyle("font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;");
 addHeader("hola", "h1", "header1");
 addParagraph("tu puta madre me da sexo a diario", "p1");
 addAudio('./test.ogg', false, "audio1");
@@ -17,5 +18,16 @@ addButton("stop", function play() {
     the.pause();
 }, "buttonstop");
 
+addButton("add button", function add() {
+    var newB = document.createElement("button");
+    newB.innerText = `Child: ${addedChilds}`;
+    newB.id = `${addedChilds}`;
+    addedChilds += 1;
+    newB.type = "button";
+    newB.addEventListener("click", (click) => {
+        alert("Just clicked: " + newB.innerText)
+    })
+    document.body.append(newB);
+});
+
 setStyle("buttonplay", "background-color: black; color: white;");
-setGlobalStyle("font-family: 'Courier New', Courier, monospace; color: white;");
