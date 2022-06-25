b00c7d444a52819d970751feccb95fd06f49632b
SanicBTW <58010536+SanicBTW@users.noreply.github.com>
2022-06-25 19:36:13 +0000

    Improve the main page (idk why)

--- index.js
+++ index.js
@@ -2,14 +2,26 @@ var elements = new ElementsHandler();
 var styles = new StylesHandler();
 
 styles.setBackground("./src/images/mean.jpeg");
+styles.setBodyStyle("font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;");
 
+if(platform == "Desktop")
+{
+    styles.setClassStyle("outer", `width: 100%; height: 98vh; display: flex; justify-content: center; `);
+}
+else
+{
+    styles.setClassStyle("outer", `width: 100%; height: 90vh; display: flex; justify-content: center; `);
+}
+
+elements.addDiv("first");
 elements.addDiv("mainDiv");
 
-styles.setBodyStyle("font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;");
-styles.setClassStyle("mainStyle", "background-color: white;");
-
-elements.setClass("mainDiv", "mainStyle");
+elements.setClass("first", "outer");
 
 elements.addHeader("Everyone is so mean 2 me", "h1", "why");
+
+elements.appendTo("mainDiv", "first");
+
+styles.setStyle("first", "align-items: center;");
 
 elements.appendTo("why", "mainDiv");
