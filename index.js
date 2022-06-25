14e8cee918ff7cadf52ae2d229a244a08cc59191
SanicBTW <58010536+SanicBTW@users.noreply.github.com>
2022-06-25 15:55:43 +0000

    Beta 0.0.16 - Added IntelliSense suggestions and set class style

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 var base = new Builder();
 base.setBackground("./src/images/mean.jpeg");
+
 base.addDiv("mainDiv");
+
 base.setBodyStyle("font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;");
+base.setClassStyle("mainStyle", "background-color: white;");
+
+base.setClass("mainDiv", "mainStyle");
+
+base.addHeader("Everyone is so mean 2 me", "h1", "why");
+
+base.appendTo("why", "mainDiv");
--- src/builder.js
+++ src/builder.js
@@ -1,6 +1,6 @@
 var addedChilds = 0;
 var platform = "";
-var ver = "BETA 0.0.15";
+var ver = "BETA 0.0.16";
 
 class Builder
 {
@@ -18,6 +18,7 @@ class Builder
             resp.text().then((gitVer) => {
                 console.log("GITHUB VERSION: " + gitVer);
                 console.log("CURRENT VERSION: " + ver);
+
                 if(ver > gitVer)
                 {
                     console.warn("[WARNING - Version checker]\nCurrent version is newer than the old version\nPush commit or check the Github version");
@@ -30,15 +31,25 @@ class Builder
                 {
                     console.error("[ERROR - Version checker]\nGithub version is newer\nCheck the current version and check the Github version");
                 }
+
             });
         });
     }
 
+    /**
+     * 
+     * @param {string} path The background path
+     */
     setBackground(path)
     {
         document.body.style = `background-image: url('${path}')`;
     }
 
+    /**
+     * 
+     * @param {string} path The image path
+     * @param {string} id The Element ID
+     */
     addImage(path, id)
     {
         var img = document.createElement("img");
@@ -48,6 +59,12 @@ class Builder
         document.body.append(img);
     }
 
+    /**
+     * 
+     * @param {string} text The text to display
+     * @param {string} type Type of heading (h1 - h2 - h3 - h4 - h5 - h6)
+     * @param {string} id 
+     */
     addHeader(text, type, id)
     {
         var header = document.createElement(type);
@@ -57,6 +74,11 @@ class Builder
         document.body.append(header);
     }
 
+    /**
+     * 
+     * @param {string} text The text to display
+     * @param {string} id The Element ID
+     */
     addParagraph(text, id)
     {
         var paragraph = document.createElement("p");
@@ -66,6 +88,12 @@ class Builder
         document.body.append(paragraph);
     }
 
+    /**
+     * 
+     * @param {string} path The audio path
+     * @param {boolean} showControls Should it show controls?
+     * @param {string} id The Element ID
+     */
     addAudio(path, showControls, id)
     {
         var audio = document.createElement("audio");
@@ -76,6 +104,12 @@ class Builder
         document.body.append(audio);
     }
 
+    /**
+     * 
+     * @param {string} text Text to display
+     * @param {function} onClick The click function you want to execute when button clicked
+     * @param {string} id The Element ID
+     */
     addButton(text, onClick, id)
     {
         var button = document.createElement("button");
@@ -89,17 +123,32 @@ class Builder
         document.body.append(button);
     }
 
+    /**
+     * 
+     * @param {string} id The Element ID to assign the style to
+     * @param {string} style The style you want to assign to the element
+     */
     setStyle(id, style)
     {
         var element = document.getElementById(id);
         element.style = style;
     }
 
+    /**
+     * 
+     * @param {string} style Add CSS styling to the body
+     */
     setBodyStyle(style)
     {
         document.body.style.cssText += style;
     }
 
+    /**
+     * 
+     * @param {string} link Where does the "A" redirects you to
+     * @param {string} text Text to display
+     * @param {string} id The Element ID
+     */
     addAnchor(link, text, id)
     {
         var anch = document.createElement("a");
@@ -110,6 +159,11 @@ class Builder
         document.body.append(anch);
     }
 
+    /**
+     * 
+     * @param {string} toAppend What is going to be appended (as a child)
+     * @param {string} where Where the element is going to be appended (as a child)
+     */
     appendTo(toAppend, where)
     {
         var appendLoc = document.getElementById(where);
@@ -117,6 +171,10 @@ class Builder
         appendLoc.appendChild(whatTo);
     }
     
+    /**
+     * 
+     * @param {string} id The Element ID
+     */
     addDiv(id)
     {
         var newDiv = document.createElement("div");
@@ -125,9 +183,26 @@ class Builder
         document.body.append(newDiv);
     }
 
+    /**
+     * 
+     * @param {string} id The Element ID you want to assign the class to
+     * @param {string} className The element class
+     */
     setClass(id, className)
     {
         var element = document.getElementById(id);
         element.className = className;
+    }
+
+    /**
+     * 
+     * @param {string} className The element class name
+     * @param {string} style What the class style should be
+     */
+    setClassStyle(className, style)
+    {
+        var newCSS = document.createElement("style");
+        newCSS.innerHTML = `.${className} { ${style} }`;
+        document.head.appendChild(newCSS);
     }
 }
