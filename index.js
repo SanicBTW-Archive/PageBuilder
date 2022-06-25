95e4ee6aa2b94946289829a97c8d5fc9e42304e8
SanicBTW <58010536+SanicBTW@users.noreply.github.com>
2022-06-25 14:55:36 +0000

    Beta 0.0.14 - Moved to class and making a version detector

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-addDiv("mainDiv");
-setBodyStyle("font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;");
+var base = new Builder();
+base.setBackground("./src/images/mean.jpeg");
+base.addDiv("mainDiv");
+base.setBodyStyle("font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;");
--- src/builder.js
+++ src/builder.js
@@ -1,101 +1,108 @@
 var addedChilds = 0;
 var platform = "";
+var ver = "BETA 0.0.14";
 
-function setBackground(path)
+class Builder
 {
-    document.body.style = `background-image: url('${path}')`;
+    constructor()
+    {
+        const detectDeviceType = () => /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+            ? 'Mobile'
+            : 'Desktop';
+        platform = detectDeviceType();
+        console.log("Platform: " + platform);
+        //check builder version
+        console.log("CURRENT VERSION: " + ver);
+    }
+
+    setBackground(path)
+    {
+        document.body.style = `background-image: url('${path}')`;
+    }
+
+    addImage(path, id)
+    {
+        var img = document.createElement("img");
+        img.src = path;
+        img.id =  `${id != null ? id : addedChilds}`;
+        addedChilds += 1;
+        document.body.append(img);
+    }
+
+    addHeader(text, type, id)
+    {
+        var header = document.createElement(type);
+        header.innerText = text;
+        header.id = `${id != null ? id : addedChilds}`;
+        addedChilds += 1;
+        document.body.append(header);
+    }
+
+    addParagraph(text, id)
+    {
+        var paragraph = document.createElement("p");
+        paragraph.innerText = text;
+        paragraph.id = `${id != null ? id : addedChilds}`;
+        addedChilds += 1;
+        document.body.append(paragraph);
+    }
+
+    addAudio(path, showControls, id)
+    {
+        var audio = document.createElement("audio");
+        audio.src = path;
+        audio.controls = showControls;
+        audio.id = `${id != null ? id : addedChilds}`;
+        addedChilds += 1;
+        document.body.append(audio);
+    }
+
+    addButton(text, onClick, id)
+    {
+        var button = document.createElement("button");
+        button.addEventListener("click", (click) => {
+            onClick();
+        });
+        button.type = "button";
+        button.innerText = text;
+        button.id = `${id != null ? id : addedChilds}`;
+        addedChilds += 1;
+        document.body.append(button);
+    }
+
+    setStyle(id, style)
+    {
+        var element = document.getElementById(id);
+        element.style = style;
+    }
+
+    setBodyStyle(style)
+    {
+        document.body.style.cssText += style;
+    }
+
+    addAnchor(link, text, id)
+    {
+        var anch = document.createElement("a");
+        anch.href = link;
+        anch.innerText = text;
+        anch.id = `${id != null ? id : addedChilds}`;
+        addedChilds += 1;
+        document.body.append(anch);
+    }
+
+    appendTo(toAppend, where)
+    {
+        var appendLoc = document.getElementById(where);
+        var whatTo = document.getElementById(toAppend);
+        appendLoc.appendChild(whatTo);
+    }
+    
+    addDiv(id)
+    {
+        var newDiv = document.createElement("div");
+        newDiv.id = `${id != null ? id : addedChilds}`;
+        addedChilds += 1;
+        document.body.append(newDiv);
+    }
 }
-
-function addImage(path, id)
-{
-    var img = document.createElement("img");
-    img.src = path;
-    img.id =  `${id != null ? id : addedChilds}`;
-    addedChilds += 1;
-    document.body.append(img);
-}
-
-function addHeader(text, type, id)
-{
-    var header = document.createElement(type);
-    header.innerText = text;
-    header.id = `${id != null ? id : addedChilds}`;
-    addedChilds += 1;
-    document.body.append(header);
-}
-
-function addParagraph(text, id)
-{
-    var paragraph = document.createElement("p");
-    paragraph.innerText = text;
-    paragraph.id = `${id != null ? id : addedChilds}`;
-    addedChilds += 1;
-    document.body.append(paragraph);
-}
-
-function addAudio(path, showControls, id)
-{
-    var audio = document.createElement("audio");
-    audio.src = path;
-    audio.controls = showControls;
-    audio.id = `${id != null ? id : addedChilds}`;
-    addedChilds += 1;
-    document.body.append(audio);
-}
-
-function addButton(text, onClick, id)
-{
-    var button = document.createElement("button");
-    button.addEventListener("click", (click) => {
-        onClick();
-    });
-    button.type = "button";
-    button.innerText = text;
-    button.id = `${id != null ? id : addedChilds}`;
-    addedChilds += 1;
-    document.body.append(button);
-}
-
-function setStyle(id, style)
-{
-    var element = document.getElementById(id);
-    element.style = style;
-}
-
-function setBodyStyle(style)
-{
-    document.body.style.cssText += style;
-}
-
-function addAnchor(link, text, id)
-{
-    var anch = document.createElement("a");
-    anch.href = link;
-    anch.innerText = text;
-    anch.id = `${id != null ? id : addedChilds}`;
-    addedChilds += 1;
-    document.body.append(anch);
-}
-
-//might improve it
-function appendTo(toAppend, where)
-{
-    var appendLoc = document.getElementById(where);
-    var whatTo = document.getElementById(toAppend);
-    appendLoc.appendChild(whatTo);
-}
-
-function addDiv(id)
-{
-    var newDiv = document.createElement("div");
-    newDiv.id = `${id != null ? id : addedChilds}`;
-    addedChilds += 1;
-    document.body.append(newDiv);
-}
-
-//from https://www.w3resource.com/javascript-exercises/fundamental/javascript-fundamental-exercise-72.php
-const detectDeviceType = () => /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
-    ? 'Mobile'
-    : 'Desktop';
-platform = detectDeviceType();
-
